94e5497313ff1dcf8995c7662eb0119004d43c42
Zet992 <rodionovalexey11@mail.ru>
2024-10-24 07:40:26 +0300

    improvement, i think

--- updates_libs.py
+++ updates_libs.py
@@ -42,13 +42,13 @@ class Graph:
             self.Graph.append(vertex)
 
 
-
 def checked_copy(l1):
     s1 = list(set(l1))
     for i in s1:
         if l1.count(i) > 2:
             return True
-        
+
+
 def perebor(graph, start, end):
     all_paths = [CurrentPath(start, end)]
     answer_paths = []
@@ -56,38 +56,28 @@ def perebor(graph, start, end):
 
     while n <= len(graph) or len(answer_paths) == 0:
         n += 1
-        temp_all_paths = []  # Нужен, т.к. all_paths в цикле изменять нельзя
+        temp_all_paths = []
         for path in all_paths:
-            current_vertex = path.last_point()
             # Проверка на Гамильтонов цикл (вершину проехать можно только один раз)
-            if checked_copy(path.path[1:]):  # Если прошелся второй раз по любой вершине,
+            if checked_copy(path.path[1:]):  # Если вершину проехали больше 2 раз
                 all_paths.remove(path)  # Удаляем путь из списка всех путей
-                continue
-            neighbours = current_vertex.neighbours  # список с кортежами сосед. вершин (номер, вес)
-            for vertex in neighbours:  # vertex - обычное число
-                # if vertex[0] == path.last_point().number:
-                #     continue  # По идее невозможная ситуация (вершина не может быть своим соседом)
+                continue  # И больше не рассматриваем данный маршрут
+
+            current_vertex = path.last_point()  # Берём последнюю точку
+            neighbours = current_vertex.neighbours  # Список с кортежами сосед. вершин (номер, вес)
+            for vertex in neighbours:
                 current_path = CurrentPath(path, end)  # Создаем новой путь
-                current_path.add_points(vertex[0], vertex[1], graph)  # И добавляем текущую вершину
-                temp_all_paths.append(current_path)
+                current_path.add_points(vertex[0], vertex[1], graph)  # Добавляем в него текущую вершину
+                temp_all_paths.append(current_path)  # И сохраняем его в общем списке с путями
                 if current_path.check():  # Если путь вернулся в начало
                     if (len(graph) == len(set(current_path.path))):  # И длина равна количеству вершин
                         answer_paths.append(current_path)  # Получаем подходящий путь
         all_paths = temp_all_paths.copy()
-    maxs = [answer_paths[0]]
-    # Не понимаю зачем. Здесь не знак '<'?
-    for path in answer_paths:
-        if len(set(path.path)) > len(set(maxs[0].path)):
-            maxs = [path]
-        elif len(set(path.path)) == len(set(maxs[0].path)):
-            maxs.append(path)
-    answer = maxs
-    if len(answer) > 1:  # Если возможных путей больше одного
-        # Выбираем наименее длинный
-        mins = [answer[0]]
-        for path in answer:
-            if path.weight < mins[0].weight:
-                mins = [path]
-            elif path.weight == mins[0].weight:
-                mins.append(path)
-    return list(map(lambda x: x.number, answer[0].path))
+
+    # Выбираем наименее длинный путь
+    shortest_answer = answer[0]
+    for path in answer[1:]:
+        if path.weight < shortest_answer.weight:
+            shortest_answer = path
+
+    return list(map(lambda x: x.number, shortest_answer.path))
